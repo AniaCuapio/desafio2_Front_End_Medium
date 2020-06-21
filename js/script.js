8a82929e234ac829197932ca4701540450f27f08
codigoVis <etneciv2@icloud.com>
2020-06-21 14:26:55 -0500

    No sirve solo para actualizar de develop

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
 
-const scrollInfinit=()=>{
-    $(window).scroll(function(){
-        totalDocumentHeight=$(document),height();
-        heightScroll=(this).scollTop();
-        heightViwport=(this).height();
-        
-         console.log(totalDocumentHeight)
-         console.log(heightScroll)
-         console.log(heightViwport)
-        })
-
-}
-
-scrollInfinit ()
 
 
