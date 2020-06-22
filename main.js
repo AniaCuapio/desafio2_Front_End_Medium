c1f4b3484ef26bfe81d05a14538729f904d72c7f
Ruben Somuano <rubasomuano.g2@gmail.com>
2020-06-22 11:58:59 -0500

    Delete-refre-eqis

--- main.js
+++ main.js
@@ -53,6 +53,9 @@ let arrayTotals = arrayPopularSection + arrayRecentSection
 
 log ("array totals:", arrayTotals)
 
+
+
+
 const printPosts=()=>{
 
 arrayRecentSection.forEach(post =>{
@@ -63,11 +66,18 @@ arrayRecentSection.forEach(post =>{
   <div class="col-8" data-id ="${id}">
     <div class="card-body py-0">
        <h5 class="card-title">${title}</h5>
-       <span><i data-id="${id}" class="fas fa-times"></i></span> </p>
-       <p class="card-text m-0">${author}</p>
+       <div class="delete-btn">
+        <svg class="bi bi-x" data-id ="${id}" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"/>
+        <path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"/>
+        </svg>
+      </div>
+      <p class="card-text m-0">${author}</p>
       <div class="row">
         <div class="col-10">
-          <p class="card-text"><small class="text-muted">${createdAt.toDateString()} · 4 min read ★</small></p>
+          <p class="card-text"><small class="text-muted">${createdAt} · 4 min read ★</small>
+            <span class="tooltiptext">Updated Jun 20</span>
+          </p>
         </div>
         <div class="col-2 d-flex justify-content-end px-0">
           <div class="d-md-none">
@@ -98,11 +108,16 @@ arrayPopularSection.forEach(post =>{
   <span class="hoover-h ml-5">${author}</span>
   <div class="row ml-5">
     <div class="m-0">
-    <p class="card-text"><small class="text-muted">${createdAt.toDateString()} • 14 min read</small>
-    <span class="middotDivider text-muted">★</span>
-    <span><i data-id="${id}" class="fas fa-times"></i></span> </p>
-  </div>
-</div>`
+      <p class="card-text"><small class="text-muted">${createdAt} • 14 min read</small>
+      <span class="middotDivider text-muted">★</span>
+      <div class="delete-btn">
+        <svg class="bi bi-x" data-id ="${id}" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"/>
+        <path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"/>
+        </svg>
+      </div>
+    </div>
+  </div>`
 $("#popular-container").append(postCard2)
 })
 
@@ -114,6 +129,12 @@ $("#popular-container").append(postCard2)
 // <div class="col-8 d-flex flex-column mt-5 pl-5">
 // <h6 class="text-muted font-weight-light">POPULAR ON MEDIUM</h6>
 // <h4 class="hoover-h">${title}</h4>
+// <div class="delete-btn">
+// <svg class="bi bi-x" data-id ="${id}" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+// <path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"/>
+// <path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"/>
+// </svg>
+// </d  iv>
 // <p class="text-muted font-weight-light">"${text.slice(0, 150)}..."</p>
 // <div class="d-flex justify-content-start">
 //     <span class="hoover-h">${author}</span>
@@ -130,7 +151,13 @@ $("#popular-container").append(postCard2)
 //     <div class="col-6 d-flex justify-content-end">
 //         <i class="far fa-bookmark mx-1 hoover-h"></i>
 //         <i class="fas fa-ellipsis-h text-muted mx-1 hoover-h"></i>
-//         <span><i data-id="${id}" class="fas fa-times"></i></span></p>
+//         <div class="delete-btn" data-id ="${id}">
+    //   <div class="delete-btn" data-id ="${id}">
+    //   <svg class="bi bi-x" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    //    <path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"/>
+    //    <path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"/>
+    //  </svg>
+    //  </div>
 //     </div>
 // </div>
 // </div>
@@ -142,7 +169,49 @@ $("#popular-container").append(postCard2)
 // $("#all-container").append(cardPost3)
 // })
 
+/*let deleteBtns= document.querySelectorAll(".delete-btn")
+deleteBtns.forEach(btn=>{
+  btn.addEventListener("click", prueba)
+})*/
+$(".delete-btn").click(prueba)
 }
+
+const prueba = (e)=>{
+  e.stopPropagation();
+  console.log(e.target)
+  let id= e.target.getAttribute("data-id")
+  console.log(id)
+  deletePost(id)
+  RefreshPage()
+  //printPosts()
+  
+  
+  //alert("prueba de borrar")
+}
+
+function RefreshPage() {
+  setTimeout(rfrsh, 1000);
+}
+
+function rfrsh() {
+  window.location.reload()
+  console.log("refreshing")
+}
+
+const deletePost = (PostId) => {
+  let xhttp = new XMLHttpRequest()
+  xhttp.onreadystatechange = function(){ 
+      if(this.readyState == 4 && this.status == 200){      
+          let response = JSON.parse(this.responseText) 
+          console.log(response)
+      }
+  }
+  xhttp.open("DELETE",`https://ajaxclass-1ca34.firebaseio.com/medium-team2/${PostId}.json`,true)
+  xhttp.send()
+  console.log("post borrado")
+}
+
+
 
 printPosts()
 
@@ -158,7 +227,7 @@ const loadPage = (selector, url, callback) => {
 const handleSaveListener = () => {
   $("#save-btn").click(function (e) {
     e.preventDefault();
-    alert("prueba de botón guardar")
+    //alert("prueba de botón guardar")f
     let title = $("#input-title").val()
     let text = $("#input-text").val()
     let author = $("#input-author").val()
@@ -167,7 +236,7 @@ const handleSaveListener = () => {
     //log(section)
     let postObject = { title, text, author, imgUrl, section}
     postData(postObject)
-
+    location.reload()
   });
 }
 
@@ -185,3 +254,4 @@ const postData = (object) => {
 }
 
 //             $(".delete").click(deleteTableRow)
+
